73256cd950c34ccc5d3967526e85d381d76b428a
Gustavo García <gugadev@MacbookPro.local>
2018-01-15 11:02:40 -0500

    pet model

---
+++ models/pet.py
@@ -0,0 +1,12 @@
+import json
+
+class Pet:
+  def __init__(self, name="", breed="", age=0, weight=0.0, photo=""):
+    self.name = name
+    self.breed = breed
+    self.age = age
+    self.weight = weight
+    self.photo = photo
+  
+  def to_json(self):
+    return json.dumps(vars(self))
