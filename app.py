c6e86aafee51ea88249262783fe4158f2100fb76
Gustavo García <gugadev@MacbookPro.local>
2018-01-15 11:03:11 -0500

    main file

---
+++ app.py
@@ -0,0 +1,10 @@
+from sanic import Sanic
+from handlers.pet import pets
+
+app = Sanic()
+
+# Registro de blueprints
+app.blueprint(pets)
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
